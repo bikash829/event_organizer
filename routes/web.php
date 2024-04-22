3b65b9f8cdb6da6639cfe1a58894c6dd40f0ea08
Bikash Chowdhury <chowdhurybikash38@gmail.com>
2024-04-22 23:56:16 +0600

    layout created

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
+// Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('something_else');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/', function () {
+    return view('home');
+})->name('home');
