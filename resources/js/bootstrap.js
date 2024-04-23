e3932e3ea631891186a6a11a42e4e89460a342c9
Bikash Chowdhury <chowdhurybikash38@gmail.com>
2024-04-23 13:37:02 +0600

    datatable has been seted up successfully

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -15,6 +15,9 @@ window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 import $ from "jquery";
 window.$ = window.jQuery = $;
 
+//Data tables
+import DataTable from "datatables.net-jqui";
+import "datatables.net-responsive-jqui";
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
