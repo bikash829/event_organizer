1b76e5249b73e0cc8793288ddebee2dc01f8e313
Bikash Chowdhury <chowdhurybikash38@gmail.com>
2024-05-09 14:24:20 +0600

    component forms has been created again

---
+++ resources/views/components/forms/input.blade.php
@@ -0,0 +1,37 @@
+@php
+    $id = $id ?? ($name ?? rand());
+@endphp
+
+
+<div class="{{ $colSize }}">
+    @if ($label)
+        <label for="input_{{ $id }}">
+
+            <strong>{{ $label }} </strong>
+            @if ($isRequired)
+                <strong class="text-danger">*</strong>
+            @endif
+        </label>
+    @endif
+    @if (in_array($type, ['text', 'password', 'email', 'number', 'date', 'datetime-local', 'hidden', 'search']))
+        <input value="{{ old($name, $value) }}" type="{{ $type }}"
+            class="form-control @error($errorKey ?? $name) is-invalid @enderror {{ $class }}"
+            id="input_{{ $id }}" name="{{ $name }}" {{ $attributes }} maxlength="{{ $maxLength }}"
+            minlength="{{ $minLength }}" placeholder="{{ $placeholder }}"
+            @if ($isRequired) required @endif>
+    @elseif ($type == 'textarea')
+        <textarea class="form-control @error($errorKey ?? $name) is-invalid @enderror" id="input_{{ $id }}"
+            name="{{ $name }}" {{ $attributes }} maxlength="{{ $maxLength }}" minlength="{{ $minLength }}"
+            placeholder="{{ $placeholder }}" @if ($isRequired) required @endif>{{ old($name, $value) }}</textarea>
+    @else
+        <div class="text-danger">
+            <span>Invalid Input Type</span>
+        </div>
+    @endif
+
+
+    @error($errorKey ?? $name)
+        <div class="error invalid-feedback">{{ $errors->first($name) }}</div>
+    @enderror
+
+</div>
